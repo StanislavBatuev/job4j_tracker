fee7fbab5d5e8f199c1246e540ec8cbab3645d49
LogOS0408 <bsv-81@mail.ru>
2024-07-06 12:07:18 +0400

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/tracker/Checkstyle/Broken.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker.Checkstyle;
+
+public class Broken {
+    private final int sizeOfEmpty = 10;
+
+    public String surname;
+
+    public static final String newValue = "";
+
+    void echo() {
+    }
+
+    void Media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+
+        void Method(int a, int b, int c, int d, int e, int f, int g) {
+
+        }
+
+        String name;
+
+        new Broken(); {
+
+        }
+    }
+}
