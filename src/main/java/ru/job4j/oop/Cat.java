1cece1c7ae014314adbf638370dd19af480410ae
LogOS0408 <bsv-81@mail.ru>
2024-07-07 10:43:28 +0400

    1.4. Вызов метода с возвращаемым типом. [#167387 #338621] _ Cat

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,15 @@
 package ru.job4j.oop;
 
 public class Cat {
+    public String sound() {
+        return "mayrg-mayrg!";
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
+        System.out.println("Peppy says - " + peppy.sound()
+                + "\n"
+                + "Peppy sparky - " + sparky.sound());
     }
 }
