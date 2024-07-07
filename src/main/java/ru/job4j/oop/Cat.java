3d63e84fce1ff4da478c86fa87f867600f08f3fa
LogOS0408 <bsv-81@mail.ru>
2024-07-07 11:56:26 +0400

    1.5. Поля объекта [#166083 #338655]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,6 +1,21 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public String sound() {
         return "mayrg-mayrg!";
     }
@@ -8,8 +23,18 @@ public class Cat {
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
+        Cat gav = new Cat();
+        Cat black = new Cat();
         System.out.println("Peppy says - " + peppy.sound()
                 + "\n"
                 + "Peppy sparky - " + sparky.sound());
+        System.out.println("There is gav's food.");
+        gav.eat("Steak");
+        gav.giveNick("Gav");
+        gav.show();
+        System.out.println("There is black's food.");
+        black.giveNick("Black");
+        black.eat("Fish");
+        black.show();
     }
 }
