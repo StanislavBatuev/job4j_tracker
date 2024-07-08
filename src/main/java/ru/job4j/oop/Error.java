87031de0677a7b54714b4eebc1c880a8214d7795
LogOS0408 <bsv-81@mail.ru>
2024-07-08 08:40:08 +0400

    1.7. Конструктор [#173889 #339431]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void print() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 445, "Error -182380");
+        error.print();
+        Error errorEmpty = new Error();
+        errorEmpty.print();
+    }
+}
