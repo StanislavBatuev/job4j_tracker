4c57d2bd31a11219a9ebb4a78ba0c98e1f90ee0b
LogOS0408 <bsv-81@mail.ru>
2024-07-17 12:19:47 +0400

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/Tracker.java
+++ src/main/java/ru/job4j/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -11,15 +13,34 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    public Item[] findAll() {
+        Item[] result = new Item[size];
+        int resultSize = 0;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
+            if (items[index] != null) {
+                result[resultSize++] = items[index];
             }
         }
-        return rsl;
+        return Arrays.copyOf(result, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int resultSize = 0;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getName().equals(key)) {
+                result[resultSize++] = items[index];
+            }
+        }
+        return Arrays.copyOf(result, resultSize);
+    }
+
+    public Item findById(int id) {
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                return items[index];
+            }
+        }
+        return null;
     }
 }
