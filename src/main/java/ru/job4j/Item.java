1127004a4bb9b02308a652a9bf2a41d044a5b034
LogOS0408 <bsv-81@mail.ru>
2024-07-19 10:05:58 +0400

    10. toString [#310018 #505731]

--- src/main/java/ru/job4j/Item.java
+++ src/main/java/ru/job4j/Item.java
@@ -6,25 +6,14 @@ import java.time.format.DateTimeFormatter;
 public class Item {
     private int id;
     private String name;
-    private LocalDateTime created;
+    private LocalDateTime created = LocalDateTime.now();
     private static final DateTimeFormatter FORMATTER
             = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-
-    public Item() {
-        this.created = LocalDateTime.now();
-    }
 
     public Item(int id, String name, LocalDateTime current) {
         this.id = id;
         this.name = name;
         this.created = current;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{"
-                + "id=" + id + ", name='" + name + '\''
-                + ", created=" + created.format(FORMATTER) + '}';
     }
 
     public Item(int id) {
@@ -38,6 +27,13 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{"
+                + "id=" + id + ", name='" + name + '\''
+                + ", created=" + created.format(FORMATTER) + '}';
     }
 
     public int getId() {
