6dad68f7183b5057a7953ee120c43a64b4608bf4
LogOS0408 <bsv-81@mail.ru>
2024-07-23 18:55:23 +0400

    3.3. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,7 +6,6 @@ import ru.job4j.Tracker;
 import java.util.Scanner;
 
 public class StartUI {
-
     public void init(Scanner scanner, Tracker tracker) {
         boolean run = true;
         while (run) {
@@ -16,12 +15,12 @@ public class StartUI {
             if (select == 0) {
                 System.out.println("=== Создание новой заявки ===");
                 System.out.print("Введите имя: ");
-                Item item = new Item(scanner.nextLine());
+                String name = scanner.nextLine();
+                Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
-            } else if (select == 6) {
-                run = false;
             } else if (select == 1) {
+                System.out.println("=== Вывод всех заявок ===");
                 Item[] items = tracker.findAll();
                 if (items.length > 0) {
                     for (Item item : items) {
@@ -30,16 +29,26 @@ public class StartUI {
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
                 }
+            } else if (select == 6) {
+                run = false;
+            } else if (select == 2) {
+                System.out.println("=== Редактирование заявки ===");
+                System.out.println("Введите id:");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.println("Введите Имя:");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                if (tracker.replace(id, item)) {
+                    System.out.println("Замена прошла успешно");
+                } else {
+                    System.out.println("Ошибка замены заявки.");
+                }
             }
         }
     }
 
     private void showMenu() {
-        String[] menu = {
-                "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
-                "Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
-                "Завершить программу"
-        };
+        String[] menu = {"Добавить новую заявку", "Показать все заявки", "Изменить заявку", "Удалить заявку", "Показать заявку по id", "Показать заявки по имени", "Завершить программу"};
         System.out.println("Меню:");
         for (int i = 0; i < menu.length; i++) {
             System.out.println(i + ". " + menu[i]);
