47b743270e78e15a0756c58f6739b7c517e8e4c8
LogOS0408 <bsv-81@mail.ru>
2024-08-05 15:42:57 +0400

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370]

---
+++ src/main/java/ru/job4j/ex/FactorialRecursion.java
@@ -0,0 +1,15 @@
+package ru.job4j.ex;
+
+public class FactorialRecursion {
+    public static int calc(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return calc(n - 1) * n;
+    }
+
+    public static void main(String[] args) {
+        int result = calc(4);
+        System.out.println(result);
+    }
+}
